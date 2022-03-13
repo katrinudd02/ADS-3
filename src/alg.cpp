f9c22c2981924a23b3ea89f7d1b378a0e06748d0
katrinudd02 <97793336+katrinudd02@users.noreply.github.com>
2022-03-13 13:57:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,31 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+#include <iostream>
+
+int cbinsearch(int* arr, int size, int value) {
+    int middle = 0;
+    int left = 0;
+    int right = size;
+    int count = 0;
+    
+    if (size % 2 == 0) {
+        middle = size / 2;
+    } else {
+        middle = ((size - 1) / 2);
+    }
+
+    if (value < arr[middle]) {
+        right = middle - 1;
+    }
+    else if (value > arr[middle]) {
+        left = middle + 1;
+    } else {
+        count += 1;
+    }
+
+    if (count > 0) {
+        return count;
+    } else {
+        return 0;
+    }
 }
